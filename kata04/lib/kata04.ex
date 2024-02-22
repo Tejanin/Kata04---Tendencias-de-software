6f0da87c0b97e1e9041b0119312243348bffcd6c
Tejanin <theexama123@gmail.com>
2024-02-22 15:03:48 -0400

    Project created

---
+++ kata04/lib/kata04.ex
@@ -0,0 +1,18 @@
+defmodule Kata04 do
+  @moduledoc """
+  Documentation for `Kata04`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kata04.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ kata04/test/kata04_test.exs
@@ -0,0 +1,8 @@
+defmodule Kata04Test do
+  use ExUnit.Case
+  doctest Kata04
+
+  test "greets the world" do
+    assert Kata04.hello() == :world
+  end
+end
