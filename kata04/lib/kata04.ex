4523f632011c01ec60526b476dcc543152084667
Tejanin <theexama123@gmail.com>
2024-02-22 15:54:17 -0400

    Now we can access and filter columns

--- kata04/lib/kata04.ex
+++ kata04/lib/kata04.ex
@@ -10,9 +10,25 @@ def convert_to_matrix(file_path) do
     String.split(row,",")
   end)
 
+  access_columns(columns,[0,1,2])
 
 end
 
+def access_columns(columns, indexes)do
+filtered_columns = Enum.map(columns, fn row ->
+  Enum.map(indexes, &Enum.at(row, &1))
+end)
 
+get_difference(filtered_columns)
+end
+
+
+def get_difference(columns) do
+  target_columns = Enum.map(columns, fn [col1, col2, col3] ->
+    {col1, String.to_integer(col2) - String.to_integer(col3)}
+  end)
+
+  IO.inspect(target_columns)
+end
 
 end
